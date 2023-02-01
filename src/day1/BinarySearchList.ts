e239eedfa745f287a9c972fee07505d881845a7f
zkpranav <>
2023-02-01 15:31:42 +0530

    Implemented binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const mid = Math.floor(low + (high - low) / 2);
+        const value = haystack[mid];
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    } while(low < high);
+
+    return false;
 }
