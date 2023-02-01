77542d32c232fdfedf342d5004a68af399ed13a4
zkpranav <>
2023-02-01 15:33:41 +0530

    Adding notes

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,8 @@
+/*
+    Notes -
+    Consider off by 1 conditions.
+    Include low & exclude high or vice versa. Depending on this, decide whether to floor or ceil the mid calculation.
+*/
 export default function bs_list(haystack: number[], needle: number): boolean {
     let low = 0;
     let high = haystack.length;
