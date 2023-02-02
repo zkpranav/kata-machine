364cdb2527cdd61390c8351277e99202c687ef53
zkpranav <>
2023-02-02 14:11:06 +0530

    Implemented stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,51 @@
+/*
+    A <-- B <-- C(h)
+    Push at the head
+    Pop at the head
+*/
+
+type SNode<T> = {
+    value: T,
+    prev?: SNode<T>
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: SNode<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = {
+            value: item
+        } as SNode<T>;
+        this.length += 1;
 
-}
+        node.prev = this.head;
+        this.head = node;
+    }
+
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length -= 1;
+        const temp = this.head;
+        this.head = this.head.prev;
+        temp.prev = undefined;
+
+        return temp.value;
+    }
+
     peek(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        return this.head.value;
+    }
 }
