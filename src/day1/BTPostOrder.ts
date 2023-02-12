e86c308f0892d17b9041893f43c730fd170dccbd
zkpranav <zkpranav@icloud.com>
2023-02-12 15:24:58 +0530

    Implemented postorder traversal

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,15 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function visit(node: BinaryNode<number> | null, path: Array<number>): number[] {
+    if (node === null) {
+        return path;
+    }
 
+    visit(node.left, path);
+    visit(node.right, path);
+    path.push(node.value);
+
+    return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return visit(head, []);
 }
