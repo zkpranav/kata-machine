86dcc299e25e4d3de47225b3b145c251eb44a984
zkpranav <zkpranav@icloud.com>
2023-02-12 15:22:24 +0530

    Implemented inorder traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,15 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function visit(node: BinaryNode<number> | null, path: Array<number>): number[] {
+    if (node === null) {
+        return path;
+    }
 
+    visit(node.left, path);
+    path.push(node.value);
+    visit(node.right, path);
+
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return visit(head, []);
 }
