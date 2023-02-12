6116757c36d68ed744e2ba88d29d659af8d27cc2
zkpranav <zkpranav@icloud.com>
2023-02-12 15:19:37 +0530

    Implemented Preorder traversal

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,27 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+/*
+    Tree traversals -
+    Pre-order -> Root, left, right (root in the beginning)
+    In-order -> Left, root, right (root in the middle)
+    Post-order -> Left, right, root (root in the end)
 
+    Generalized -
+    Pre-order - Parent then children
+    Post-order - Children then parent
+*/
+
+function visit(node: BinaryNode<number> | null, path: Array<number>): number[] {
+    if (node === null) {
+        return path;
+    }
+
+    path.push(node.value);
+
+    visit(node.left, path);
+    visit(node.right, path);
+
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return visit(head, []);
 }
