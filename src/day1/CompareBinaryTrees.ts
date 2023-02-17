f23d17c61f2f23dc27b094535cd796e43d9524c9
zkpranav <zkpranav@icloud.com>
2023-02-17 14:50:28 +0530

    Implemented DFS to compare two binary trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,19 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+/*
+    Prefer a depth-first search here, or a similar stack oriented search because it preserves shape
+*/
 
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    if (a === null || b == null) {
+        if (a === b) {
+            return true;
+        } else  {
+            return false;
+        }
+    }
+
+    if (a.value !== b.value) {
+        return false;
+    } else {
+        return compare(a.left, b.left) && compare(a.right, b.right);
+    }
 }
