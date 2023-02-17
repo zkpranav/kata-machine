f3118e8aa829f123963d371b97460768ab6c307e
zkpranav <zkpranav@icloud.com>
2023-02-17 14:28:53 +0530

    Implemented breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,24 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+/*
+    A tree order traversal, where you visit each level of the tree completely before proceeding to the next.
+*/
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q: BinaryNode<number>[] = [head];
+
+    while (q.length) {
+        const node = q.shift() as BinaryNode<number>;
+
+        if (node.value === needle) {
+            return true;
+        }
+
+        if (node.left) {
+            q.unshift(node.left);
+        }
+        if (node.right) {
+            q.unshift(node.right);
+        }
+    }
+
+    return false;
 }
