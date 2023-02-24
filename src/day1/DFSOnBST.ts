77e65879876f3034e235b1e1dd86820e8b03c581
zkpranav <zkpranav@icloud.com>
2023-02-24 19:46:50 +0530

    Implemented DFS on a BST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,17 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(node: BinaryNode<number> | null, needle: number): boolean {
+    if (!node) {
+        return false;
+    }
 
+    if (needle < node.value) {
+        return search(node.left, needle);
+    } else if (needle > node.value) {
+        return search(node.right, needle);
+    } else {
+        return true;
+    }
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
 }
