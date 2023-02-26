0d9c36518cfbd70877b869d92fa5aa06f33b5c08
zkpranav <zkpranav@icloud.com>
2023-02-26 15:44:51 +0530

    Implemented BFS on a graph

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,49 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+/*
+    BFS is reminiscent of a implicit queue
+    Done similarly as done on a tree
+*/
 
+export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    // Useful for walking backwards to derive the path
+    const prev: number[] = new Array(graph.length).fill(-1);
+
+    seen[source] = true;
+    const q: number[] = [source];
+
+    do {
+        const curr = q.shift() as number;
+        if (curr === needle) {
+            break;
+        }
+        
+        const adjs = graph[curr];
+        for (let i = 0; i < adjs.length; i++) {
+            if (adjs[i] === 0 || seen[i]) {
+                continue;
+            }
+
+            seen[i] = true;
+            q.push(i);
+            // Create link for walking back
+            prev[i] = curr;
+        }
+
+    } while (q.length > 0);
+
+    // Walk back
+    let curr = needle;
+    const out: number[] = [];
+
+    while (prev[curr] !== -1) {
+        out.push(curr);
+        curr = prev[curr];
+    }
+
+    if (out.length) {
+        out.push(source);
+        return out.reverse();
+    } else {
+        return null;
+    }
 }
