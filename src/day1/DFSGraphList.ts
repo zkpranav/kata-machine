f7b00f6eb5fdcaecebb1197c6344b511b14f5f3f
zkpranav <zkpranav@icloud.com>
2023-02-26 17:24:35 +0530

    Implemented DFS on a graph

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,34 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(graph: WeightedAdjacencyList, curr: number, needle: number, seen: boolean[], path: number[]): boolean {
+    if (seen[curr]) {
+        return false;
+    }
+    seen[curr] = true;
+    path.push(curr);
 
+    if (curr === needle) {
+        return true;
+    }
+
+    const adjs = graph[curr];
+    for (let i = 0; i < adjs.length; i++) {
+        const ret = walk(graph, adjs[i].to, needle, seen, path);
+        if (ret) {
+            return true;
+        }
+    }
+
+    path.pop();
+    return false;
+}
+
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const path: number[] = [];
+    const seen: boolean[] = new Array(graph.length).fill(false);
+
+    const ret = walk(graph, source, needle, seen, path);
+    if (ret) {
+        return path;
+    } else {
+        return null;
+    }
 }
