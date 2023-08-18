ab21a6acef17b45dea7a15511fe7d2da0ed2c494
zkpranav <zkpranav@icloud.com>
2023-08-18 17:21:51 +0530

    Binary search

--- src/day2/BinarySearchList.ts
+++ src/day2/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const mid = Math.floor(((high - low) / 2) + low);
+        const value = haystack[mid];
+
+        if (needle === value) {
+            return true;
+        } else if (needle > value) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    } while (low < high);
+
+    return false;
 }
