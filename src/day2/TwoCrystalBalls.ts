223e1d64c53c9e917ca4d45067e9b60d009f9b59
zkpranav <zkpranav@icloud.com>
2023-08-18 17:35:59 +0530

    Two crystal balls

--- src/day2/TwoCrystalBalls.ts
+++ src/day2/TwoCrystalBalls.ts
@@ -1,3 +1,36 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    if (breaks.length === 0) {
+        return -1;
+    }
 
+    const jump = Math.floor(Math.sqrt(breaks.length));
+    let pos = 0;
+    while (pos < breaks.length) {
+        if (breaks[pos] === true) {
+            let i = pos - jump;
+            while (i < pos) {
+                if (breaks[i] === true) {
+                    return i;
+                }
+                i += 1;
+            }
+
+            return pos;
+        }
+
+        pos += jump;
+    }
+
+    if (pos - breaks.length < jump) {
+        pos = pos - jump + 1;
+        while (pos < breaks.length) {
+            if (breaks[pos] === true) {
+                return pos;
+            }
+
+            pos += 1;
+        }
+    }
+
+    return -1;
 }
